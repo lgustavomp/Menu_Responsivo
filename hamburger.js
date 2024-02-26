d5f2e3751d4496818eef59f4e4499ca1760f65e2
lgustavomp <lgustavomp@hotmail.com>
2024-02-26 11:23:21 -0300

    02_26_24_01

--- hamburger.js
+++ hamburger.js
@@ -2,16 +2,23 @@ const hamburger = document.getElementById('hamburger')
 const menu = document.getElementById('menu')
 const header = document.getElementById('header')
 const item = document.querySelectorAll('.menu_item')
+const body = document.querySelector('body')
 
 hamburger.addEventListener("click", () => {
+    header.classList.toggle('active_header')
+    body.classList.toggle('active_body')
     menu.classList.toggle('active')
     hamburger.classList.toggle('active_ham')
+
+   
+
 })
 
 item.forEach(e => {
     e.addEventListener("click", () => {
+        header.classList.remove('active_header')
+        body.classList.remove('active_body')
         menu.classList.remove('active')
         hamburger.classList.remove('active_ham')
     })
 }) 
-    
